4e9c40b8281f9ecd35821cee5193d058e884c211
Ying Wang <ywang@xometry.com>
2017-07-06 10:50:08 -0400

    Editing class files.

---
+++ src/main/java/classes/BasicTicketService.java
@@ -0,0 +1,15 @@
+public class BasicTicketService implements TicketService {
+
+    public int numSeatsAvailable() {
+        return 0;
+    }
+
+    public BasicSeatHold findAndHoldSeats(int numSeats, String customerEmail) {
+        return null;
+    }
+
+    public String reserveSeats(int seatHoldId, String customerEmail) {
+        return null;
+    }
+
+}
