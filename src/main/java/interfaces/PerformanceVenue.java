9d6511884b8cc126df939d54b711fe178c3ba565
Ying Wang <ywang@xometry.com>
2017-07-06 17:36:34 -0400

    Adding changes.

--- src/main/java/interfaces/PerformanceVenue.java
+++ src/main/java/interfaces/PerformanceVenue.java
@@ -13,20 +13,21 @@ public interface PerformanceVenue {
 
     /**
     * Reserve exactly one seat in the PerformanceVenue.
-    * Returns true if the seat has been successfully marked as reserved.
-    * Returns false if the seat could not be successfully marked as reserved.
+    * @param seatId: the ID of the Seat to be marked as reserved.
+    * @return: true if marking is successful, false if not successful.
     */
     boolean markSeatAsReserved(int seatId);
 
     /**
     * Hold exactly one seat in the PerformanceVenue.
-    * Returns true if the seat has been successfully marked as held.
-    * Returns false if the seat could not be successfully marked as held.
+    * @param seatId: the ID of the Seat to be marked as reserved.
+    * @return: true if marking is successful, false if not successful.
     */
     boolean markSeatAsHeld(int seatId);
 
     /**
-    * Get the number of available seats within the PerformanceVenue.
+    * Gets number of available seats.
+    * @return: number of available seats.
     */
     int getNumAvailableSeats();
 
