eec31df70f8e1f79fd4e5a113b70c15b95260916
Ying Wang <yingw787@gmail.com>
2017-07-06 20:35:17 -0400

    Adding more documentation.

--- src/main/java/interfaces/Seat.java
+++ src/main/java/interfaces/Seat.java
@@ -8,12 +8,16 @@ public interface Seat {
 
     /**
     * Hold Seat.
-    * @param holderId: the ID of the holder that will hold the seat.
+    * @param holderId: the ID of the holder that will hold the Seat.
     */
     void hold(String holderId);
 
     /**
     * Reserve Seat.
+    * Without args, is reserving Seat without holding it.
+    * With args, is reserving Seat after holding it. Can only be done if
+    * claimerId matches the Seat's holderId.
+    * @param claimerId: the ID of the claimer that will reserve the Seat.
     */
     void reserve();
     void reserve(String claimerId);
